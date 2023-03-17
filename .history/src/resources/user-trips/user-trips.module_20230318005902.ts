52e2556ffdc003fe6e770f618c74925ef0a3af65
itumeleng-falcorp <itumelengtshabalala8@gmail.com>
2023-03-18 01:20:32 +0200

    Removed history files

--- .history/src/resources/user-trips/user-trips.module_20230318005902.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserTripsService } from './user-trips.service';
-import { UserTripsController } from './user-trips.controller';
-
-@Module({
-  controllers: [UserTripsController],
-  providers: [UserTripsService]
-})
-export class UserTripsModule {}
